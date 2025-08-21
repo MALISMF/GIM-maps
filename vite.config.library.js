e833a7ca73e0603c4d45f46d5daf5dc08eff18ee
Boris Salimov <boris@work-istp.ru>
2025-08-21 16:12:05 +0800

    Add copying style.css

--- vite.config.library.js
+++ vite.config.library.js
@@ -1,9 +1,31 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
+import fs from 'fs'
+import { fileURLToPath } from 'url'
+
+// Определяем __dirname в модульной системе ES
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+     vue(),
+     {
+      name: 'copy-style-css',
+      closeBundle() {
+        const src = path.resolve(__dirname, 'src/style.css')
+        const dest = path.resolve(__dirname, 'dist-library/style.css')
+        
+        // Создаём папку dist-library, если её нет
+        fs.mkdirSync(path.dirname(dest), { recursive: true })
+        
+        fs.copyFileSync(src, dest)
+        
+        console.log('✓ style.css copied to dist-library/')
+      }
+    }
+  ],
   build: {
     lib: {
       entry: path.resolve(__dirname, 'src/App.vue'),
